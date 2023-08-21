350d50e6f61109f99497c68c37485986be066c25
Karsten Aichholz <karsten.aichholz@mailbox.org>
2023-08-21 12:43:01 -0700

    Initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (inputString, howOften) {
+    if (howOften < 0) {
+        return "ERROR"
+    } else {
+        return inputString.repeat(howOften);
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = "";
+    for (let i = 0; i < inputString.length; i++) {
+        outputString = outputString + inputString.charAt(inputString.length - 1 - i)
+        console.log(outputString);
+    };
+    return outputString;
 };
 
 // Do not edit below this line
